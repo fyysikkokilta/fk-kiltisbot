6fbeeaca81fafa1ac7dfd5e44efea9f47621163a
Ari Viitala <amt.viitala@gmail.com>
2020-01-24 19:45:00 +0200

    Now most of the fiirumi side stuff should be implemented

--- fiirumi.py
+++ fiirumi.py
@@ -18,8 +18,8 @@ def get_posts_after(time):
     res = requests.get(url)
     data = json.loads(res.text)
 
-    posts = [(x["name"], x["username"], x["created_at"], 
-              x["topic_title"], x["topic_slug"], x["category_d"]) 
+    posts = [(x["name"], x["username"], x["created_at"],
+              x["topic_title"], x["topic_slug"], x["category_d"])
               for x in data["latest_posts"] if data["created_at"] > time]
 
     return posts
@@ -31,7 +31,7 @@ def check_messages(bot):
     keyboard = InlineKeyboardMarkup([[👍,😂,😍,🎉,🙈,💩]])
 
     data = load_data()
-    
+
     posts = get_posts_after(data["previous_messages"])
     data["previous_messages"] = datetime.datetime.utcnow().isoformat()
 
@@ -40,9 +40,34 @@ def check_messages(bot):
             msg = format_message(p)
             if not (p["category_id"] == lorina_id and c["name"] == "Fyysikkokilta"):
                 text = format_message(p)
-                bot.send_message(c["id"], text, reply_markup=keyboard, parse_mode="MARKDOWN")
+                msg = bot.send_message(c["id"], text, reply_markup=keyboard, parse_mode="MARKDOWN")
+                data["sent_messages"].append({"username": p["username"], "chat": msg.chat.id, "message": msg.message_id})
 
-def voting
+
+    save_data(data)
+
+def vote_message(bot, update):
+
+    global data
+    data = load_data()
+
+    message = update.message.effective_message.id
+    chat = update.message.effective_chat.id
+
+    emoji = update.callback_query.data.split(" ")[-1]
+    username = [x["username"] for x in data["sent_messages"] if data["chat"] == chat and data["message"] == message][0]
+
+    index = [data.index(x) for x in data["users"] if x["username"] == username][0]
+    user = data["users"][index]
+
+    try:
+        user["emojis"][emoji] += 1
+    except KeyError:
+        user["emojis"][emoji] = 1
+
+    data["users"][index] = user
+
+    save_data(data)
 
 def format_message(post):
 
@@ -57,21 +82,21 @@ def format_message(post):
     else:
         emojis = user[0]["emojis"]
 
-    emoji_string = "".join([str(emojis[key]) for key in emojis])
+    emoji_string = " " +  "".join([str(emojis[key]) for key in emojis])
 
-    text = "Uusi postaus Φiirumilla!\n\n *{}*\n _{}_({} {}) \n\n [Lue koko postaus]({})"
-    text = text.format(post["topic_title"], post["name"], post["username"], 
+    text = "Uusi postaus Φiirumilla!\n\n *{}*\n _{}_({}{}) \n\n [Lue koko postaus]({})"
+    text = text.format(post["topic_title"], post["name"], post["username"],
                        emoji_string, base_url + post["topic_slug"])
 
     return text
-    
+
 
 def subscribe(bot, update):
     global data
     data = load_data()
-    data["chats"].append({"name": update.effective_chat.name, "id": update.update.effective_chat.id})
+    data["chats"].append({"name": update.effective_chat.title, "id": update.update.effective_chat.id})
     save_data(data)
-    
+
 
 def load_data():
     global data
