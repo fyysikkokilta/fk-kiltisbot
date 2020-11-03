2213259d79645edf4378154ef82fce6e0034db5d
summis <summanentommi@gmail.com>
2020-11-03 16:25:30 +0200

    Get rid of some warnings in logs
    by setting cache_discovery to false. See also calendar module.

--- db/drive.py
+++ db/drive.py
@@ -12,14 +12,12 @@ import google_auth
 
 
 def import_inventory():
-    service = build('sheets', 'v4', credentials=google_auth.creds)
-
+    service = build('sheets', 'v4', credentials=google_auth.creds, cache_discovery=False)
     sheet = service.spreadsheets()
     result = sheet.values().get(spreadsheetId=config.INVENTORY_SHEET_ID, range="A1:3", majorDimension = "COLUMNS").execute()
     values = result.get('values', [])
     values = list(map(lambda x: [x[0], int(x[1]), int(x[2])], values[1:]))
-
-    print(values)
+    print("Importing inventory: ", values)
     db.delete_inventory()
     for i in values:
         db.add_item(i[0], i[1], i[2])
@@ -27,49 +25,35 @@ def import_inventory():
 
 
 def import_users():
-    service = build('sheets', 'v4', credentials=google_auth.creds)
-
+    service = build('sheets', 'v4', credentials=google_auth.creds, cache_discovery=False)
     sheet = service.spreadsheets()
     result = sheet.values().get(spreadsheetId=config.USERS_SHEET_ID, range="A1:D", majorDimension = "ROWS").execute()
     values = result.get('values', [])
     values = list(map(lambda x: [x[0], x[1], x[2], int(x[3])], values))
-
     users = len(db.get_users())
-    print(values)
-
+    print("Importing user data: ", values)
     export_users() #safety export to not lose data
-
     db.delete_users()
     for i in values:
         db.add_user(i[0], i[1], i[2], i[3])
-
     return len(db.get_users()) - users
 
 
+# TODO get rid of headers in google sheets to simplify things
 def import_transactions():
-    service = build('sheets', 'v4', credentials=google_auth.creds)
-
+    service = build('sheets', 'v4', credentials=google_auth.creds, cache_discovery=False)
     sheet = service.spreadsheets()
     result = sheet.values().get(spreadsheetId=config.TRANSACTIONS_SHEET_ID, range="A1:F").execute()["values"]
-    print(result[:10])
     values = list(map(lambda x: (int(x[1]), x[3], x[5], int(float(x[4]))), result[1:]))
-    print(values[:10])
-    #values = list(map(lambda x: (x[1], x[3], x[5], x[4]), result))
-
+    print("Importing transactions (first 10 shown): ", values[:10])
     db.delete_transactions()
-
     for i in values:
-        try:
-            db.add_transaction(int(i[0]), None, i[1], i[2], int(i[3]))
-        except:
-            pass
-            #print(i)
-
+        db.add_transaction(int(i[0]), None, i[1], i[2], int(i[3]))
     return len(values)
 
 
 def export_inventory():
-    service = build('sheets', 'v4', credentials=google_auth.creds)
+    service = build('sheets', 'v4', credentials=google_auth.creds, cache_discovery=False)
 
     inventory = list(map(lambda x: str(x[0]), db.get_stocks()))
 
@@ -84,7 +68,7 @@ def export_inventory():
 
 
 def export_users():
-    service = build('sheets', 'v4', credentials=google_auth.creds)
+    service = build('sheets', 'v4', credentials=google_auth.creds, cache_discovery=False)
     sheet = service.spreadsheets()
 
     date = datetime.datetime.today().isoformat()[:16].replace(":", ".")
@@ -123,7 +107,7 @@ def export_users():
 
 
 def export_transactions():
-    service = build('sheets', 'v4', credentials=google_auth.creds)
+    service = build('sheets', 'v4', credentials=google_auth.creds, cache_discovery=False)
 
     sheet = service.spreadsheets()
     result = sheet.values().get(spreadsheetId=config.TRANSACTIONS_SHEET_ID, range="A1:F").execute()
